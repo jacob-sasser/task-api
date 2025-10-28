c7014f782ae90d3b66cdad5ae12ec3da13b641b9
jacob-sasser <jacobsasser23@gmail.com>
2025-10-28 16:58:29 -0400

    a

--- src/server.js
+++ src/server.js
@@ -9,6 +9,9 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 
+const specs=YAML.load('../docs/openapi.yaml');
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(specs));
+
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
